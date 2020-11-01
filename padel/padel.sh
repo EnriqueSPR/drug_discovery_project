53482b3157c5bf14da0ab4694b07f46ebfd6ddb5
Enrique Alcalde <enriquealcaldegrau@gmail.com>
2020-11-01 20:26:49 +0100

    adds files

---
+++ padel/padel.sh
@@ -0,0 +1,1 @@
+java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv
